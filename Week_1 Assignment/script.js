5382dad5c166ed963abd7f7ad86d68e69c146be3
alex <abravov@wgu.edu>
2023-10-02 11:23:15 -0700

    added an intro

---
+++ Week_1 Assignment/script.js
@@ -0,0 +1,4 @@
+let firstName = "Alejandro";
+let lastName = "Bravo-Vasquez";
+let yearsOfStudy = "2 quarters";
+let goal = "Gain enough experience to launch my own web design and application business";
